b76db3eb6da6a57cedc66665a1a80ca2bd8369d7
josenerio2 <35904629+josenerio2@users.noreply.github.com>
2018-03-08 13:38:56 -0600

    Update project-no-3.py

--- project-no-3.py
+++ project-no-3.py
@@ -42,72 +42,52 @@ for line in fh:
         part = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
         if len(part)>=7:
             dt=datetime.strptime(part[1], "%d/%b/%Y")
+            x=(part[1].split("/"))
+            y=x[1]
+
+            if y=='Jan':
+                months_req[0]+=1
+            elif y=='Feb':
+                months_req[1]+=1
+            elif y=='Mar':
+                months_req[2]+=1
+            elif y=='Apr':
+                months_req[3]+=1
+            elif y=='May':
+                months_req[4]+=1
+            elif y=='Jun':
+                months_req[5]+=1
+            elif y=='Jul':
+                months_req[6]+=1
+            elif y=='Aug':
+                months_req[7]+=1
+            elif y=='Sep':
+                months_req[8]+=1
+            elif y=='Oct':
+                months_req[9]+=1
+            elif y=='Nov':
+                months_req[10]+=1
+            elif y=='Dec':
+                months_req[11]+=1
+
+            days_req[dt.weekday()]+=1
             
             if int(part[6])>=300 and int(part[6])<=399:
                 tfers+=1
-            if int(part[6])>400 and int(part[6])<=499:
+            if int(part[6])>=400 and int(part[6])<=499:
                 dead+=1
-               
+            #if x[1]=='Jan':
+            #    months_req[0]+=1
             total_req+=1
-#print (un/total_req)
-#print (suc/total_req)
-print (total_req)            
-
-    part = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
-    if len(part)>=7:
-        dt=datetime.strptime(part[1], "%d/%b/%Y")
         
-        #splits=part
-        #months_req[dt.month]
-        #if months_req[0]==1:
-        #    months_req[0]+=1
-        #days_req[dt.weekday()] += 1
-        x=(part[1].split("/"))
-        y=x[1]
-        
-        if y=='Jan':
-            months_req[0]+=1
-        elif y=='Feb':
-            months_req[1]+=1
-        elif y=='Mar':
-            months_req[2]+=1
-        elif y=='Apr':
-            months_req[3]+=1
-        elif y=='May':
-            months_req[4]+=1
-        elif y=='Jun':
-            months_req[5]+=1
-        elif y=='Jul':
-            months_req[6]+=1
-        elif y=='Aug':
-            months_req[7]+=1
-        elif y=='Sep':
-            months_req[8]+=1
-        elif y=='Oct':
-            months_req[9]+=1
-        elif y=='Nov':
-            months_req[10]+=1
-        elif y=='Dec':
-            months_req[11]+=1
-        
-        days_req[dt.weekday()]+=1
-        if int(part[6])>=300 and int(part[6])<=399:
-            tfers+=1
-        if int(part[6])>=400 and int(part[6])<=499:
-            dead+=1
-        #if x[1]=='Jan':
-        #    months_req[0]+=1
-        total_req+=1
-        
-       # print(y)
-if not os.path.exists(mon_file[dt.month]):
-    file = open(mon_file[dt.month]), "w")
-    file.write(line)
-    file.close
-else:
-    file = open(mon_file[dt.month]), "a")
-    file.write(line)
-    file.close
+        if not os.path.exists(mon_file[dt.month]):
+            file = open(mon_file[dt.month]), "w")
+            file.write(line)
+            file.close
+        else:
+            file = open(mon_file[dt.month]), "a")
+            file.write(line)
+            file.close
 #print (total_req)            
 #print (tfers)
 #print (dead)
